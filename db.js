6bb1abe819da43c00bcfaf262e6c1a9825188385
Brandon Botsch <brandon.botsch@gmail.com>
2017-08-09 19:38:44 -0400

    Add basic API calls for db plus some backend helper functions

--- db.js
+++ db.js
@@ -3,8 +3,63 @@ const mongoCollections = require("./mongoCollections");
 
 const students = mongoCollections.students;
 const teachers = mongoCollections.teachers;
-const courses = mongoCollectinos.classes;
+const courses = mongoCollectinos.courses;
 const assignments = mongoCollections.assignments;
+
+
+function addCourseToTeacher(teacherId, courseId) {
+	if (typeof teacherId != "string") {
+		return Promise.reject("StudentId must be a string");
+	}
+	if (typeof courseId != "string") {
+		return Promise.reject("ClassId must be a string");
+	}
+
+	let courseInfo = {
+		courseId: courseId,
+		isCurrentlyTeaching: true
+	};
+
+	return teachers().then((collection) => {
+		return collection.update({_id: teacherId}, {$addToSet: {courses: courseInfo}});
+	});
+}
+
+function addCourse(newCourse) {
+	if (typeof newCourse != "object") {
+		return Promise.reject("Course must be provided");
+	}
+
+	newCourse._id = uuid();
+	newCourse.studentIDs = [];
+	newCourse.assignments = [];
+
+	return courses().then((collection) => {
+		return collection.insertOne(newCourse).then((information) => {
+			return information.insertedId;
+		});
+	});
+}
+
+function addAssignment(newAssignment) {
+	if (typeof newAssignment != "object") {
+		return Promise.reject("Assignment must be provided");
+	}
+
+	newAssignment._id = uuid();
+	
+	return assignments().then((collection) => {
+		return collection.insertOne(newAssignment);
+	});
+}
+
+function addAssignmentToCourse(courseId, assignmentId) {
+	return Promise.reject("Not yet implemented");
+}
+
+function updateCourseGrade(studentId, courseId, grade) {
+	return Promise.reject("Not yet implemented");
+}
 
 module.exports = {
 	addStudent(newStudent) {
@@ -29,7 +84,7 @@ module.exports = {
 			return collection.insertOne(newTeacher);
 		});
 	},
-	addStudentCourse(studentId, courseId) {
+	addStudentToCourse(studentId, courseId) {
 		if (typeof studentId != "string") {
 			return Promise.reject("StudentId must be a string");
 		}
@@ -41,52 +96,32 @@ module.exports = {
 			courseId: courseId,
 			grade: undefined,
 			isCurrentlyTaking: true
-		};
+		}
 
 		return students().then((collection) => {
 			return collection.update({_id: studentId}, {$addToSet: {courses: courseInfo}});
 		});
 	},
-	addTeacherCourse(teacherId, courseId) {
-		if (typeof teacherId != "string") {
-			return Promise.reject("StudentId must be a string");
-		}
-		if (typeof courseId != "string") {
-			return Promise.reject("ClassId must be a string");
-		}
-
-		let courseInfo = {
-			courseId: courseId,
-			isCurrentlyTeaching: true
-		};
-
-		return teachers().then((collection) => {
-			return collection.update({_id: teacherId}, {$addToSet: {courses: courseInfo}});
+	createCourseForTeacher(teacherId, newCourse) {
+		addCourse(newCourse).then((courseId) => {
+			return addCourseToTeacher(teacherId, courseId);
 		});
 	},
-	addCourse(newCourse) {
-		if (typeof newCourse != "object") {
-			return Promise.reject("Course must be provided");
-		}
-
-		newCourse._id = uuid();
-		newCourse.studentIDs = [];
-		newCourse.assignments = [];
-
-		return courses().then((collection) => {
-			return collection.insertOne(newCourse);
+	createAssignmentForCourse(courseId, newAssignment) {
+		addAssigment(newAssignment).then((assignmentId) => {
+			return addAssignmentToCourse(courseId, assignmentId);
 		});
 	},
-	addAssignment(newAssignment) {
-		if (typeof newAssignment != "object") {
-			return Promise.reject("Assignment must be provided");
-		}
-
-		newAssignment._id = uuid();
-		teacherResponse: undefined;
-
-		return assignments().then((collection) => {
-			return collection.insertOne(newAssignment);
-		});
+	getAssignmentsForCourse(courseId) {
+		return Promise.reject("Not yet implemented");
+	},
+	getCoursesForAssignment(assignmentId) {
+		return Promise.reject("Not yet implemeneted");
+	},
+	updateAssignmentGrade(studentId, assignmentId, grade, teacherResponse) {
+		return Promise.reject("Not yet implemented");
+	},
+	updateAssignmentSubmission(studentId, assignmentId, submission) {
+		return Promise.reject("Not yet implemented");
 	}
 }
