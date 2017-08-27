0a9051772991ccbc4af5a9803efb33d298e5d9e1
avi <pillyavi@yahoo.com>
2017-08-27 08:20:41 -0400

    html validation

--- public/js/creation.js
+++ public/js/creation.js
@@ -1,10 +1,19 @@
 
 const pass = $("#password")[0];
 const cpass = $("#confirm-password")[0];
+const id = $("#ID")[0];
 const check = () => {
     if (pass.value !== cpass.value) {
         cpass.setCustomValidity("Confirm Password does not match old password");
     } else {
         cpass.setCustomValidity("");
+    }
+}
+const valid = () => {
+    let value = Number(id.value);
+    if (Number.isInteger(value) && id.value.length === 8) {
+        id.setCustomValidity("");
+    } else {
+        id.setCustomValidity('Id must be 8 numerical characters');
     }
 }
